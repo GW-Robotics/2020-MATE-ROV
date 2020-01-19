3e9288c3d031d5d5c969c85dc0a0fba900703b3a
nbury <42916102+nbury@users.noreply.github.com>
2020-01-19 12:17:05 -0500

    Update robotserver.py

--- robotserver.py
+++ robotserver.py
@@ -4,9 +4,8 @@ from gpiozero import Servo
 
 host = '169.254.159.7'
 port = 1234
-motor0 = Servo(0,initial_value = -1)
-
-motor1 = Servo(1,initial_value = -1)
+motor0 = Servo(0,initial_value = None)
+motor1 = Servo(1,initial_value = None)
 
 def readInput(data):
 	x_axis = data[0]
@@ -22,11 +21,17 @@ conn, addr=s.accept()
 print("Conn: ",conn," Address: ",addr)
 
 x_axis = -1
-
+init = false
 while True:
 	data = pickle.loads(conn.recv(1024))
 	d = readInput(data)
-	motor0.value = d
-	motor1.value = d
+	if d is not 0
+		if not init:
+			motor0.value = -1
+			motor1.value = -1
+		else:
+			motor0.value = d
+			motor1.value = d
+	
 	send = ("Recieved: " + str(d))
 	print(send)
