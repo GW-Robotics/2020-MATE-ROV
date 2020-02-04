205922346d1f16c1fb2c7ecf4a80c0aa6dcfb04b
Rick Sear <searri98@gmail.com>
2020-02-04 17:57:31 -0500

    Motor test works with 8 motors

--- motor_test/motor_test.ino
+++ motor_test/motor_test.ino
@@ -1,14 +1,22 @@
 #include <Servo.h> 
 
-#define ESC_0 9
-#define ESC_1 10
-#define ESC_2 11
-#define ESC_3 12
+#define ESC_0 6
+#define ESC_1 7
+#define ESC_2 8
+#define ESC_3 9
+#define ESC_4 10
+#define ESC_5 11
+#define ESC_6 12
+#define ESC_7 13
 
 Servo esc0;
 Servo esc1;
 Servo esc2;
 Servo esc3;
+Servo esc4;
+Servo esc5;
+Servo esc6;
+Servo esc7;
 
 int minPulseRate = 1000;
 int maxPulseRate = 2000;
@@ -24,12 +32,20 @@ void setup() {
   esc1.attach(ESC_1, minPulseRate, maxPulseRate);
   esc2.attach(ESC_2, minPulseRate, maxPulseRate);
   esc3.attach(ESC_3, minPulseRate, maxPulseRate);
+  esc4.attach(ESC_4, minPulseRate, maxPulseRate);
+  esc5.attach(ESC_5, minPulseRate, maxPulseRate);
+  esc6.attach(ESC_6, minPulseRate, maxPulseRate);
+  esc7.attach(ESC_7, minPulseRate, maxPulseRate);
   
   // Write a minimum value (most ESCs require this correct startup)
   esc0.write(0);
   esc1.write(0);
   esc2.write(0);
   esc3.write(0);
+  esc4.write(0);
+  esc5.write(0);
+  esc6.write(0);
+  esc7.write(0);
   
 }
 
@@ -68,6 +84,10 @@ void changeThrottle(int throttle) {
     esc1.write(currentThrottle + step);
     esc2.write(currentThrottle + step);
     esc3.write(currentThrottle + step);
+    esc4.write(currentThrottle + step);
+    esc5.write(currentThrottle + step);
+    esc6.write(currentThrottle + step);
+    esc7.write(currentThrottle + step);
     currentThrottle = readThrottle();
     delay(throttleChangeDelay);
   }
