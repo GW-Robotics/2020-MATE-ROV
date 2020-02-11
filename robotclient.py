bd6a805ae02cd1cace63f67678eff8fb50e1ba3e
Rick Sear <searri98@gmail.com>
2020-02-11 17:34:15 -0500

    Update server code from 2/9

--- robotclient.py
+++ robotclient.py
@@ -3,13 +3,12 @@ import sys
 import pygame
 import os
 
-# host = "127.0.0.1"
-host = '169.254.159.7'
+host = "127.0.0.1"
 port = 1234
 
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-print("CLient Socket Successfully created!")
-s.connect((host,port))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("Client Socket Successfully created!")
+s.connect((host, port))
 
 pygame.init()
 screen = pygame.display.set_mode((400, 300))
@@ -19,8 +18,8 @@ pygame.joystick.init()
 joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
 
 if len(joysticks) == 0:
-	print('Joystick not detected')
-	exit()
+    print("Joystick not detected")
+    exit()
 
 joystick = joysticks[0]
 joystick.init()
@@ -29,49 +28,53 @@ clock = pygame.time.Clock()
 
 done = False
 
-#Manual controls -> Keyboard numbers
-#Up -> R trigger (A:5678)
-#Down -> L trigger (D:5678)
-#Roll right -> R bumper(A:56 D:78)
-#Roll left -> L bumper (A:78 D:56)
-#Lateral movement -> L stick
-	#Forward -> (A:12 D:34)
-	#Backward -> (A:34 D:12)
-	#Left -> (A:13 D:24)
-	#Right -> (A:24 D:13)
-#Tilt(U,D) and Turn(L,R) - R stick
-	#Up -> (A:68 D:57)
-	#Down -> (A:57 D:68)
-	#Left -> (A:1 D:234)
-	#Right -> (A:2 D:134)
+# Manual controls -> Keyboard numbers
+# Up -> R trigger (A:5678)
+# Down -> L trigger (D:5678)
+# Roll right -> R bumper(A:56 D:78)
+# Roll left -> L bumper (A:78 D:56)
+# Lateral movement -> L stick
+# Forward -> (A:12 D:34)
+# Backward -> (A:34 D:12)
+# Left -> (A:13 D:24)
+# Right -> (A:24 D:13)
+# Tilt(U,D) and Turn(L,R) - R stick
+# Up -> (A:68 D:57)
+# Down -> (A:57 D:68)
+# Left -> (A:1 D:234)
+# Right -> (A:2 D:134)
 
 motorInputs = []
 
 while not done:
-	for event in pygame.event.get(): # User did something.
-		if event.type == pygame.QUIT: # If user clicked close.
-                	done = True # Flag that we are done so we exit this loop.
-		elif event.type == pygame.JOYBUTTONDOWN:
-			pass
-                        # print("Joystick button pressed.")
-		elif event.type == pygame.JOYBUTTONUP:
-			pass
-                        # print("Joystick button released.")
+    for event in pygame.event.get():  # User did something.
+        if event.type == pygame.QUIT:  # If user clicked close.
+            done = True  # Flag that we are done so we exit this loop.
+        elif event.type == pygame.JOYBUTTONDOWN:
+            pass
+            # print("Joystick button pressed.")
+        elif event.type == pygame.JOYBUTTONUP:
+            pass
+            # print("Joystick button released.")
 
-	ax0 = joystick.get_axis(0)
-	ax1 = joystick.get_axis(1)
-	ax2 = joystick.get_axis(2)
-	ax3 = joystick.get_axis(3)
-	ax4 = joystick.get_axis(4)
-	bt4 = joystick.get_button(4)
-	bt5 = joystick.get_button(5)
+    ax0 = joystick.get_axis(0)
+    ax1 = joystick.get_axis(1)
+    ax2 = joystick.get_axis(2)
+    ax3 = joystick.get_axis(3)
+    ax4 = joystick.get_axis(4)
+    bt4 = joystick.get_button(4)
+    bt5 = joystick.get_button(5)
 
-	d = [ax0,ax1,ax2,ax3,ax4,bt4,bt5,1]
-	d = [round(i*100) + 100 for i in d]
+    d = [ax0, ax1, ax2, ax3, ax4, bt4, bt5, 1]
 
-	s.recv(1)
+    d = [round(i * 100) + 100 for i in d]
 
-	# data = pickle.dumps(d)
-	s.sendall(bytearray(d))
+    print(d)
+
+    bytearr = bytearray(d)
+
+    # data = pickle.dumps(d)
+    s.sendall(bytearr)
+    s.recv(1)
 
 
--- robotserver.py
+++ robotserver.py
@@ -1,12 +1,16 @@
 import socket, pickle
 import time
-from gpiozero import Servo
-
-#host = '127.0.0.1'
-host = '169.254.159.7'
+# import serial
+# from gpiozero import Servo
+# from smbus import SMBus
+# bus = SMBus(1)
+addr = 9
+host = '127.0.0.1'
 port = 1234
+n_motors = 8
 
-motor1 = Servo(1,initial_value = -1)
+# motor1 = Servo(1,initial_value = None)
+# motor1.min()
 #motor2 = Servo(2,initial_value = -1)
 #motor3 = Servo(3,initial_value = -1)
 #motor4 = Servo(4,initial_value = -1)
@@ -19,7 +23,7 @@ motor1 = Servo(1,initial_value = -1)
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.bind((host,port))
 print("Server Socket was created and bound!")
-s.listen(1)
+s.listen(5)
 
 conn, addr=s.accept()
 print("Conn: ",conn," Address: ",addr)
@@ -48,20 +52,21 @@ ax4 = 4
 bt4 = 5
 bt5 = 6
 
-motors = [motor1,motor2,motor3,motor4,motor5,motor6,motor7,motor8]
+# motors = [motor1,motor2,motor3,motor4,motor5,motor6,motor7,motor8]
+# motors = [motor1]
 
 motorInputs = [
 	{	
 		"inputs": [ax0,ax1,ax4], 
-		"mults":[1,1,1]
+		"mults":[-1,-1,1]
 	},
 	{	
 		"inputs": [ax0,ax1,ax4], 
-		"mults":[1,1,1]
+		"mults":[1,-1,1]
 	},
 	{	
 		"inputs": [ax0,ax1], 
-		"mults":[1,1]
+		"mults":[-1,1]
 	},
 	{	
 		"inputs": [ax0,ax1], 
@@ -86,23 +91,31 @@ motorInputs = [
 ]
 
 while True:
-	data = conn.recv(2048)
-	data.seek(0)
-	d = pickle.loads(data)
-
-	for i in range(0,8):
+	d = list(conn.recv(8))
+	d = [(x-100)/100 for x in d]
+	motorSend = []
+	for i in range(n_motors):
 		motorSum = 0
 		activeInputs = 0
 		for j in range(0,len(motorInputs[i]["inputs"])):
 			currentInput = d[motorInputs[i]["inputs"][j]]
+			# print(currentInput, end=" ")
 			if abs(currentInput) > .05:
-				motorSum +=  currentInput * d[motorInputs[i]["mults"][j]]
+				motorSum +=  currentInput * motorInputs[i]["mults"][j]
 				activeInputs += 1
 		if (activeInputs != 0):
 			motorSum = motorSum / activeInputs
-			print("Motor " + str(i+1) + ": " + str(motorSum*180))
-		motors[i].value = motorSum * 180	
+			# print("Motor " + str(i+1) + ": " + str(motorSum))
+		motorSend.append(motorSum)
+		
+	final_send = []
+	for value in motorSend:
+		if value < 0:
+			final_send.append(0)
+		else:
+			final_send.append(value*180)
 	
-	send = ("Recieved: " + str(d))
-	print(send)
-
+	print(final_send)
+	conn.sendall(bytes(1))
+	#bus.write_i2c_block_data(addr, 0x00, motorSend) # switch it on
+	# print()
