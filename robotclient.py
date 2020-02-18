b98af45fb1c23432a0591f1e3256a3316633ab7f
nbury <nbury@gwu.edu>
2020-02-18 18:05:01 -0500

    new input methods

--- robotclient.py
+++ robotclient.py
@@ -1,33 +1,16 @@
 import socket, pickle
 import sys
-import pygame
+from inputs import KEYS_AND_BUTTONS as codes
+from inputs import get_gamepad
 import os
 
 host = "127.0.0.1"
 port = 1234
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print("Client Socket Successfully created!")
-s.connect((host, port))
-
-pygame.init()
-screen = pygame.display.set_mode((400, 300))
-
-pygame.joystick.init()
-
-joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
-
-if len(joysticks) == 0:
-    print("Joystick not detected")
-    exit()
-
-joystick = joysticks[0]
-joystick.init()
-
-clock = pygame.time.Clock()
-
+#s.connect((host, port))
 done = False
-
 # Manual controls -> Keyboard numbers
 # Up -> R trigger (A:5678)
 # Down -> L trigger (D:5678)
@@ -45,36 +28,56 @@ done = False
 # Right -> (A:2 D:134)
 
 motorInputs = []
-
+ax0 = 0
+ax1 = 0
+ax3 = 0
+tl = 0
+tr = 0
+ax4 = 0
+bt4 = 0
+bt5 = 0
 while not done:
-    for event in pygame.event.get():  # User did something.
-        if event.type == pygame.QUIT:  # If user clicked close.
-            done = True  # Flag that we are done so we exit this loop.
-        elif event.type == pygame.JOYBUTTONDOWN:
-            pass
-            # print("Joystick button pressed.")
-        elif event.type == pygame.JOYBUTTONUP:
-            pass
-            # print("Joystick button released.")
 
-    ax0 = joystick.get_axis(0)
-    ax1 = joystick.get_axis(1)
-    ax2 = joystick.get_axis(2)
-    ax3 = joystick.get_axis(3)
-    ax4 = joystick.get_axis(4)
-    bt4 = joystick.get_button(4)
-    bt5 = joystick.get_button(5)
+    for event in get_gamepad():  # User did something.
+        code = event.code
+        value = event.state
+        if(code == "ABS_X"):
+            ax0 = (value/32768)*100
+        elif(code == "ABS_Y"):
+            ax1 = (value/32768)*100
+        elif(code == "ABS_RX"):
+            ax3 = (value/32768)*100
+        elif(code == "ABS_RY"):
+            ax4 = (value/32768)*100
+        elif(code == "BTN_TL"):
+            bt4 = value
+        elif(code == "BTN_TR"):
+            bt5 = value
+        elif(code == "ABS_Z"):
+            tl = value
+        elif(code == "ABS_RZ"):
+            tr = value
+    
 
-    d = [ax0, ax1, ax2, ax3, ax4, bt4, bt5, 1]
+        
 
-    d = [round(i * 100) + 100 for i in d]
+
+
+
+    d = [ax0, ax1, tl,tr,ax3,ax4, bt4, bt5]
+
+    d[0] = round(d[0])+100
+    d[1] = round(d[1])+100
+    d[4] = round(d[4])+100
+    d[5] = round(d[5])+100
+
 
     print(d)
 
-    bytearr = bytearray(d)
+    #bytearr = bytearray(d)
 
     # data = pickle.dumps(d)
-    s.sendall(bytearr)
-    s.recv(1)
+    #s.sendall(bytearr)
+    #s.recv(1)
 
 
